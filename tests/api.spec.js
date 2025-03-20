5c64364098e8432f861cf17e23dfa99edfd879e7
am191 <asnatemezale@gmail.com>
2025-03-20 21:22:22 +0200

    Card api test suite

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -2,7 +2,6 @@ import { test, expect } from '@playwright/test';
 //import { faker } from '@faker-js/faker' //generates random data
 import { API_KEY, API_TOKEN } from '../auth';
 
-const AUTH = `?key=${API_KEY}&token=${API_TOKEN}` 
 const ROUTE = 'https://api.trello.com/1'
 
 //pre-created items
@@ -42,4 +41,69 @@ test.describe('List API tests', async() => {
 
       expect(archiveList.ok()).toBeTruthy()
   })
+})
+
+
+test.describe('Card API tests', async() => {
+  let createCardBody
+
+  test('Get cards for a list', async({request}) => {
+      const getCards = await request.get(ROUTE+`/lists/${progressListId}/cards`, {
+          params: {
+              'key':API_KEY,
+              'token':API_TOKEN
+          }
+      })
+
+      console.log(await getCards.json())
+      expect(getCards.ok()).toBeTruthy()
+  })
+
+  test('Create and delete new card', async({request}) => {
+      const createCard = await request.post(ROUTE+'/cards', {
+          headers: {
+              "Accept": "application/json"
+          },
+          params: {
+              'key':API_KEY,
+              'token':API_TOKEN,
+              'idList': testListId,
+              'name':cardNames[0]
+          }
+      })
+      createCardBody = await createCard.json()
+  
+      expect(createCard.ok()).toBeTruthy()
+      expect(createCardBody.name).toBe(cardNames[0])
+      expect(createCardBody.idList).toBe(testListId)
+
+      //delete created card
+      const deleteCard = await request.delete(ROUTE+`/cards/${createCardBody.id}`, {
+          params: {
+              'key':API_KEY,
+              'token':API_TOKEN
+          }
+      })
+  
+      expect(deleteCard.ok()).toBeTruthy()
+  })
+  
+  test('Edit card name and description', async({request}) => {
+      const randomText = faker.lorem.sentence()
+
+      const editCard = await request.put(ROUTE+`/cards/${testCardId}`, {
+          params: {
+              'key':API_KEY,
+              'token':API_TOKEN,
+              'name': timestamp,
+              'desc':randomText
+          }
+      })
+      const editCardBody = await editCard.json()
+
+      expect(editCard.ok()).toBeTruthy()
+      expect(editCardBody.name).toBe(timestamp)
+      expect(editCardBody.desc).toBe(randomText)
+  })
+      
 })
